5eb16d0606dde0e467ba655a290b60ab6d4ec995
Rafael de Souza Martins <rafael.martins2@raizen.com>
2022-09-23 16:32:44 -0300

    feat: set validations to course dto

--- backend/src/courses/dto/create-course.dto.ts
+++ backend/src/courses/dto/create-course.dto.ts
@@ -1,1 +1,18 @@
-export class CreateCourseDto {}
+import { IsDateString, IsNumber, IsString } from "class-validator"
+
+export class CreateCourseDto {
+    @IsString()
+    courseName: string
+
+    @IsNumber()
+    teacherId: number
+
+    @IsNumber()
+    classRoomId: number
+
+    @IsDateString()
+    timeStart: string
+
+    @IsDateString()
+    timeEnd: string
+}
