4cc3dd2a5bcb2d1c95de4f1d09fc0d5e3880b321
Rafael Souza <faelzsouza@gmail.com>
2022-09-24 17:27:45 -0300

    feat: add `UsersModule` to imports list

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { CoursesModule } from './courses/courses.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [CoursesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
