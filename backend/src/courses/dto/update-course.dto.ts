7aa411b188a8f3d82e1e153f8b848fa887be4e26
Rafael Souza <faelzsouza@gmail.com>
2022-09-24 17:34:53 -0300

    feat: add `scheduleId` to dto when update course details

--- backend/src/courses/dto/update-course.dto.ts
+++ backend/src/courses/dto/update-course.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNumber, IsOptional } from 'class-validator';
 import { CreateCourseDto } from './create-course.dto';
 
-export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
+export class UpdateCourseDto extends PartialType(CreateCourseDto) {
+    @IsNumber()
+    @IsOptional()
+    scheduleId?: number
+
+}
