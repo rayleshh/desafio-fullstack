9b75a5140e9944d3c759523b7ae4b95dfcb2b6bb
Rafael Martins <faelzsouza@gmail.com>
2022-09-30 17:18:10 -0300

    feat: set browser router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import Home from './pages/Home';
+import { Container } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
+import RouterConfig from './navigation/RouterConfig';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Container>
+        <BrowserRouter>
+          <RouterConfig />
+        </BrowserRouter>
+      </Container>
     </div>
   );
 }
